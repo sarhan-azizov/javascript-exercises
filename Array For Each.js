6b7e1784c8b38a4ad3f7fcb06020559fc5915ebf
Sarhan <sarhan1988@meta.ua>
2017-08-02 18:11:02 +0300

    Update Array For Each.js

--- Array For Each.js
+++ Array For Each.js
@@ -7,3 +7,16 @@ Array.prototype.myForEach = function (fn) {
         fn.call(this, array[i], i, array);
     }
 };
+
+
+Array.prototype.myForEach = function(cb, ctx) {
+	let i = 0;
+	const ln = this.length;
+	
+	for (i; i < ln; i++) {
+		cb.call(ctx, this[i], i, this);
+    }
+}
+
+[1, 2, 3].myForEach((i, idx, arr) => 
+	console.log(i,idx,arr));
