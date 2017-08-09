0d6431cc02581b5ca1d927f00de982b5fa2dd176
Sarhan <sarhan1988@meta.ua>
2017-08-09 18:39:14 +0300

    Update Find Prime.js

--- Find Prime.js
+++ Find Prime.js
@@ -11,23 +11,3 @@ function isPrime(n) {
 [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 
  113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199].map(isPrime)
 
-function findPrime() {
-    var i,
-        j,
-        eliminated = {},
-        prime = [];
-
-    for (i = 2; i <= 5000; i++) {
-        for (j = 2; j <= 10000 / i; j++) {
-            eliminated[i * j] = true;
-        }
-    }
-
-    for (i = 2; i <= 10000; i++) {
-        if (!eliminated[i]) {
-            prime.push(i);
-        }
-    }
-
-    return prime;
-}
